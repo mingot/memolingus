49a26aaa1fe147f947de52591ea8325b5bcf12cc
Josep Marc Mingot Hidalgo <jm.mingot@gmail.com>
2012-12-14 19:00:04 +0100

    added distinction in choices showed depending if words after parsing or for reviewing profile

--- parsapp/forms.py
+++ parsapp/forms.py
@@ -3,16 +3,16 @@ from parsapp.models import WordContext
 
 
 class ParseForm(forms.Form):
-	parse_text = forms.CharField(widget=forms.Textarea, label="Introduce the text to learn from")
+	parse_text = forms.CharField(widget=forms.Textarea(attrs={'style':'margin: 0px; width: 742px; height: 301px;'}), label="Introduce the text to learn from")
 
 class WordContextForm(forms.ModelForm):
-	status = forms.ChoiceField(choices = [(1,"Ignore"),(2,"Learned"),(3,"To learn")])
+	status = forms.ChoiceField(choices = [(2,"Learned"),(3,"To learn")])
 	class Meta:
 		model = WordContext
 		fields = ('status',)
 	def __init__(self, *args, **kwargs):
 		super(WordContextForm, self).__init__(*args, **kwargs)
 		self.fields['status'].label = self.instance.word.name
-		# TODO: if status==0, choices Ignore,To Learn. If status==2,3, choices Learned, To Learn
+		if self.instance.status == 0:
+			self.fields['status'].choices=[(1,"Ignore"),(3,"To learn")]
 
-	
